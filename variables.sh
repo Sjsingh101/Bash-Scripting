87dd32aed30685224b16a1fdbe9b36a4f3f01c60
root <root@localhost.localdomain>
2019-10-15 07:10:40 +0530

    input and varible

---
+++ variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+test="70"
+
+echo $test
+
+read name
+
+echo  "your name is " $name
+
+echo  "enter first number " 
+read num1
+echo  "enter second number " 
+read num2
+
+echo $num1 + $num2
+
+
+
